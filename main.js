434836e690f4ffd84129159f52ca5d28fb2b03e7
RoxyOksy <roksana.dp.ua@gmail.com>
2018-08-08 18:27:46 +0300

    Init the project

---
+++ main.js
@@ -0,0 +1,72 @@
+var cart = {};
+
+window.onload = function () {   
+
+    var request = new XMLHttpRequest ();
+    request.open ('GET', 'goods.json');
+    request.send (null);
+    request.onreadystatechange = function () {
+        if (this.readyState === 4) {
+            if (this.status === 200) {
+                var goods = JSON.parse (this.responseText);                
+                showGoods(goods);
+            } else {
+                console.log ('Some error has happend');
+            }
+        }
+    }
+
+    loadCart();
+       
+}
+
+function showGoods(goods) {
+    var result = '';
+    for (var key in goods) {
+        result += `<div class="card">`;
+        result += `<p class="name">${goods[key].name}</p>`;
+        result += `<img src="${goods[key].img}" alt="${goods[key].name}">`;
+        result += `<div class="cost">Price: $${goods[key].price}</div>`;
+        result += `<button class="addToCart" data-id="${key}">Add to cart</button>`;
+        result += `</div>`;
+    }
+    var card = document.querySelector('.goods');
+    card.innerHTML = result;
+
+    card.onclick = function(event){
+        var target = event.target;
+        if(target.tagName != 'BUTTON'){
+            return;
+        }
+        getClick(target);        
+    };
+
+}
+
+function getClick(target){    
+    var button = target.parentNode.querySelector('.card button');
+    addToCart(button);
+}
+
+function addToCart (button){    
+    var id = button.getAttribute('data-id');
+
+    if (cart[id] === undefined) {
+        cart[id] = 1;
+    } else {
+        cart[id]++;
+    }
+    
+    saveToCart();
+}
+
+function saveToCart(){
+    localStorage.setItem('cart', JSON.stringify(cart));
+}
+
+function loadCart(){
+    var savedCart= localStorage.getItem('cart');
+    if (savedCart) {
+        cart = JSON.parse(savedCart);
+    }
+}
---
+++ shoppingCart.js
@@ -0,0 +1,101 @@
+var cart = {};
+
+window.onload = function () {  
+    loadCart();
+}
+
+function loadCart(){    
+    cart = JSON.parse(localStorage.getItem('cart'));       
+    if (isEmpty(cart)) {
+        return false;
+    } else {      
+        getGoods();
+        return true;
+    }
+}
+
+function getGoods() {
+    var request = new XMLHttpRequest ();
+    request.open ('GET', 'goods.json');
+    request.send (null);
+    request.onreadystatechange = function () {
+        if (this.readyState === 4) {
+            if (this.status === 200) {
+                var goods = JSON.parse (this.responseText);           
+                showShoppingCart(goods);
+                return goods;                
+            } else {
+                console.log ('Some error has happend');
+                return false;
+            }
+        }
+    }    
+}
+
+function showShoppingCart(goods){
+    if (isEmpty(cart)){
+        return false;
+    } else {
+        var result = '';
+        for (var key in cart) {
+            result += `<p>`;
+            result += `<button data-id="${key}" style="color: red">x</button>`;
+            result += `<img src="${goods[key].img}" alt="${goods[key].name}">`;
+            result += `<span>${goods[key].name}</span>`;
+            result += `<span>${cart[key]}</span>`;
+            result += `</p>`;
+        }
+    }
+
+    showResult(result, '.shoppingCart');
+    showResult('<a href="main.html">Continue shopping</a>', 'header');
+
+    addListener();
+    
+    return result;
+}
+
+function isEmpty(obj) {
+    console.log(obj);
+    for (var key in obj) {
+        if (obj.hasOwnProperty(key)) {            
+            return false;
+        }
+    }
+    showResult('The shopping cart is empty.<br><a href="main.html">Go shopping!</a>', '.shoppingCart');
+    showResult('', 'header');
+    return true;    
+}
+
+function addListener () {
+    var shoppingBag = document.querySelector('.shoppingCart');
+    shoppingBag.onclick = function(event){
+        var target = event.target;
+        if(target.tagName != 'BUTTON'){
+            return;
+        }
+        getClick(target);               
+    };
+}
+
+function getClick(target){    
+    var button = target.parentNode.querySelector('.shoppingCart button');
+    deleteItem(button);
+
+}
+
+function deleteItem(button) {
+    var id = button.getAttribute('data-id');
+    delete cart[id];    
+    saveToCart();
+    loadCart();    
+}
+
+function saveToCart(){
+    localStorage.setItem('cart', JSON.stringify(cart));
+}
+
+function showResult (result, querySelector) {
+    var htmlNode = document.querySelector(querySelector);
+    htmlNode.innerHTML = result;
+}
